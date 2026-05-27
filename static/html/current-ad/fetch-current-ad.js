272090db3f3d870eae3994cbdcf75f4fb25f7258
TechIntegrity Database <business@techintegrity.hu>
2026-05-27 23:04:50 +0200

    Fixed again

--- static/html/current-ad/fetch-current-ad.js
+++ static/html/current-ad/fetch-current-ad.js
@@ -1,7 +1,14 @@
 // TechIntegrity AD System
 
-fetch('https://database.techintegrity.hu/static/ads/petition.html') // Current ad
-    .then(response => response.text())
-    .then(data => {
-    document.getElementById('current-ad').innerHTML = data;
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('https://database.techintegrity.hu/static/ads/petition.html') // Current ad
+        .then(response => response.text())
+        .then(data => {
+            const container = document.getElementById('current-ad');
+            if (container) {
+                container.innerHTML = data;
+            }
+        })
+        .catch(error => console.error("Hiba a reklám betöltésekor:", error)
+    );
 });
